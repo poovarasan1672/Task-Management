0783b815375a6d30f21e73458babc8924f38b653
venkatesh_selvaraj <venkatesh.selvaraj@softsuave.com>
2025-06-26 18:36:34 +0530

    Updated Coded changes

--- task-management-frontend/src/app/app.component.ts
+++ task-management-frontend/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SidebarComponent, MatSidenavModule, MatToolbarModule],
+  imports: [RouterOutlet,SidebarComponent, MatSidenavModule, MatToolbarModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- task-management-frontend/src/app/pages/dashboard/dashboard.component.ts
+++ task-management-frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,27 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../../core/task.service';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dashboard',
   standalone:true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule,DragDropModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
 
-  totalTasks: number = 0;
-  completedTasks: number = 0;
-  overdueTasks: number = 0;
+  cards = [
+    { title: 'Total Tasks', count: 0, class: 'total', filter: 'all' },
+    { title: 'Completed', count: 0, class: 'completed', filter: 'completed' },
+    { title: 'Pending', count: 0, class: 'pending', filter: 'pending' },
+    { title: 'Overdue', count: 0, class: 'overdue', filter: 'overdue' }
+  ];
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private router: Router) {}
 
   ngOnInit() {
-    this.taskService.getTaskStats().subscribe((res:any) => {
-      this.totalTasks = res.data.totalTasks;
-      this.completedTasks = res.data.completedTasks;
-      this.overdueTasks = res.data.overdueTasks;
+    this.taskService.getTaskStats().subscribe((res: any) => {
+      this.cards[0].count = res.data.totalTasks;
+      this.cards[1].count = res.data.completedTasks;
+      this.cards[2].count = res.data.pendingTasks;
+      this.cards[3].count = res.data.overdueTasks;
     });
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  }
+
+  navigateToTasks(filter:string){
+    this.router.navigate(['/tasks'], { queryParams: { filter } });
+
   }
 }
